843cce2835ecd73ec2695458ea40e29c4aad5919
alexuschristensen <alexusdc6@gmail.com>
2022-07-12 19:05:00 -0700

    making sure I saved before closing - adding enemy names

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -5,7 +5,17 @@ var playerAttack = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+    console.log(enemyNames);
+    console.log(enemyNames[0]);
+    console.log(enemyNames[1]);
+    console.log(enemyNames[2]);
+    console.log(enemyNames.length);
+    for(var i = 0; i < enemyNames.length; i++) {
+        console.log(enemyNames[i]);
+        console.log(i);
+        console.log(enemyNames[i] + " is at " + i + " index");
+      }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -59,4 +69,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert("You need to choose a valid option. Try again!");
   }
 }
-fight();
+//fight();
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
